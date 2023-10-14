8c054403b7dcff13a60e73f2148e7801a6a0b250
Lilia <liliabendjeddou9@gmail.com>
2023-10-14 09:52:06 +0100

    refactor(layout): add navbar and footer to the layout file

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,17 @@
 import * as React from "react";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Layout({ children }) {
     // Put Header or Footer around the children element
     // Example
-    // return (
-    //     <>
-    //         <Navbar />
-    //         {children}
-    //         <Footer />
-    //     </>
-    // );
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    );
 
-    return <>{children}</>;
+    // return <>{children}</>;
 }
