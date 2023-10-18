57497a41100f914f4182cfbd6d5ba20755f221a4
mohamed <matassi.mohamed89@gmail.com>
2023-10-18 11:59:28 +0100

    feat(create a hero section): create a hero section as slide show
    create a hero section as slide show contain slides of therapist sessions, headline, subheadline and
    button.
    
    fix #22

---
+++ src/components/Home/HeroSection.jsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { images } from "@/util/library";
+
+import Button from "../elements/Buttons/Button";
+
+function HeroSection() {
+    const settings = {
+        dots: false,
+        fade: true,
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 0,
+    };
+    return (
+        <Slider {...settings} className='bg-GreenLight'>
+            {images.map((image) => (
+                <div key={image.id}>
+                    <Image
+                        className='object-cover w-full max-h-[80vh]'
+                        width={400}
+                        height={400}
+                        src={image.imgSrc}
+                        alt='Therapist Sessions'
+                    />
+                    <div className=' flex justify-between absolute bottom-1.5 text-left left-0 right-0 p-4 bg-opacity-50 bg-white bg-clip-padding'>
+                        <div className=''>
+                            <h2 className='text-lg md:text-xl lg:text-3xl sm:text-md text-DarkTeal font-extrabold font-atkinson'>
+                                Therapy at Your Fingertips: Access the
+                                World&apos;s Largest Network of Therapists
+                            </h2>
+                            <p className='hidden sm:block text-lg md:text-xl lg:text-2xl font-opensans'>
+                                Get the same quality care as in-office therapy,
+                                but with the convenience and flexibility of
+                                online.
+                            </p>
+                        </div>
+                        <div className='px-auto pt-4 mx-auto'>
+                            <Button />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </Slider>
+    );
+}
+
+export default HeroSection;
---
+++ src/util/library.js
@@ -0,0 +1,70 @@
+export const images = [
+    {
+        id: 1,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/uBaE00/za/3475714.jpg",
+    },
+    {
+        id: 2,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/HVAM00/za/5283595.jpg",
+    },
+    {
+        id: 3,
+        // type: "image",
+        imgSrc: "https://c.stocksy.com/a/GEqF00/za/3775692.jpg",
+    },
+    // {
+    //     id: 4,
+    //     type: "video",
+    //     imgSrc: "https://c.stocksy.com/a/VZAM00/y2/5283857.mp4",
+    // },
+    {
+        id: 4,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/VSOH00/za/4145599.jpg",
+    },
+    {
+        id: 5,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/uBaE00/za/3475714.jpg",
+    },
+    {
+        id: 6,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/EUzL00/za/5241246.jpg",
+    },
+    {
+        id: 7,
+        type: "image",
+        imgSrc: "https://c.stocksy.com/a/cBaE00/za/3475696.jpg",
+    },
+];
+
+export const teamMembers = [
+    {
+        name: "Maria JR",
+        //   linkedin: "",
+        image: "https://www.talkspace.com/blog/wp-content/uploads/2020/03/psychologist-psychiatrist-vs-therapist-1-800x500.jpg",
+    },
+    {
+        name: "Marris Adikwu",
+        //   linkedin: "",
+        image: "https://www.talkspace.com/blog/wp-content/uploads/2020/06/Marris_Headshot-150x150.jpg",
+    },
+    {
+        name: "Bisma Anwar",
+        //   linkedin: "",
+        image: "https://www.talkspace.com/blog/wp-content/uploads/2020/09/Bisma_Circle-min-1-150x150.png",
+    },
+    {
+        name: "Joseph Rauch",
+        //   linkedin: "",
+        image: "https://www.talkspace.com/blog/wp-content/uploads/2020/01/Joseph-Rauch.png",
+    },
+    {
+        name: "Jor-El Caraballo",
+        //   linkedin: "",
+        image: "https://www.talkspace.com/blog/wp-content/uploads/2020/06/Jor-El-Caraballo-150x150.jpg",
+    },
+];
