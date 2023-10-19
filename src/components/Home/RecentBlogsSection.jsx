9804ff69b7a8063306f678ed995ef8d3c557a51f
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-10-19 19:30:14 +0100

    feat(homepage): add slider to show recent blog articles and write missing test cases
    add slider to show recent blog articles and write missing test cases
    
    fix #25

---
+++ src/components/Home/RecentBlogsSection.jsx
@@ -0,0 +1,99 @@
+import Slider from "react-slick";
+import Card from "../Blog/SuggestedBlogsCard/Card";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const RecentBlogsSection = () => {
+    const blogs = [
+        {
+            title: "Mental Health and technology",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+            url: "",
+            blogImg: "blog-img-placeholder.svg",
+        },
+        {
+            title: "Mental Health and technology",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+            url: "",
+            blogImg: "blog-img-placeholder.svg",
+        },
+        {
+            title: "Mental Health and technology",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+            url: "",
+            blogImg: "blog-img-placeholder.svg",
+        },
+        {
+            title: "Mental Health and technology",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+            url: "",
+            blogImg: "blog-img-placeholder.svg",
+        },
+        {
+            title: "Mental Health and technology",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem",
+            url: "",
+            blogImg: "blog-img-placeholder.svg",
+        },
+    ];
+
+    const settings = {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 3500,
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+    return (
+        <div className='container max-w-[90%] mx-auto p-5'>
+            <h2 className='font-normal block text-3xl md:text-4xl rtl:md:text-3xl  rtl:lg:text-4xl uppercase break-words text-black/80 mb-5  md:mb-10'>
+                Recent Blogs
+            </h2>
+
+            <Slider {...settings} className='p-1 m-1 md:p-5 md:m-5'>
+                {blogs.map((blog, index) => (
+                    <Card
+                        key={index}
+                        title={blog.title}
+                        url={blog.url}
+                        text={blog.body}
+                        imgSrc={blog.blogImg}
+                    />
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default RecentBlogsSection;
