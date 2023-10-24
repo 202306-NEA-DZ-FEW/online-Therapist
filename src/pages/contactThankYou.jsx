63a8d425a8148c441af2b5d165c1d1e7563eedc0
HabchiSidAli <sidalihabchi96@gmail.com>
2023-10-24 18:25:32 +0100

    fix(contact): add contact translation
    add contact and thank you pages arabic translation, change image in contact page, resize image in
    contact page, fix minor padding issues in both contact and thank you pages.
    
    #65

--- src/pages/contactThankYou.jsx
+++ src/pages/contactThankYou.jsx
@@ -1,23 +1,40 @@
 import Button from "@/components/Button";
+import Layout from "@/layout/Layout";
 import Link from "next/link";
 import React from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
-function contactThankYou() {
+function ContactThankYou() {
+    const { t } = useTranslation("common");
+
     return (
-        <main className='mx-8 mt-5 lg:mx-20 lg:mt-10 font-atkinson'>
-            <h1 className='text-4xl lg:text-5xl font-medium pb-4 bg-LightBeige lg:bg-inherit text-center p-3 lg:text-left lg:p-0 rounded-lg'>
-                THANK YOU!
-            </h1>
-            <p className='text-xl lg:text-2xl text-center lg:text-left text-gray-500 my-5'>
-                Your request has been sent, a Inner Space representative will
-                get in contact with you through the email you provided as soon
-                as possible.
-            </p>
-            <Link href='/' className='mx-auto'>
-                <Button buttonText='Take me home!' buttonSize='lg' />
-            </Link>
-        </main>
+        <Layout>
+            <main className='mx-8 mt-5 lg:mx-20 lg:mt-10 h-[60vh] font-atkinson'>
+                <h1 className='text-4xl lg:text-5xl font-medium pb-4 bg-LightBeige lg:bg-inherit text-center p-3 lg:text-left lg:p-0 rounded-lg rtl:lg:text-right'>
+                    {t("contactThankYou.title")}
+                </h1>
+                <p className='text-xl lg:text-2xl text-center lg:text-left text-gray-500 my-5 rtl:lg:text-right'>
+                    {t("contactThankYou.description")}
+                </p>
+                <Link href='/' className='mx-auto'>
+                    <Button
+                        buttonText={t("contactThankYou.button")}
+                        buttonSize='lg'
+                    />
+                </Link>
+            </main>
+        </Layout>
     );
 }
 
-export default contactThankYou;
+export default ContactThankYou;
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            // Will be passed to the page component as props
+        },
+    };
+}
