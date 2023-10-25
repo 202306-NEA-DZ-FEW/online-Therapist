76b5e51e1e5cd17e08dc7c85ff74d620dfeec64c
Lilia <liliabendjeddou9@gmail.com>
2023-10-25 22:11:27 +0100

    feat(buy ticket page): creat buy ticket page and payment card component

---
+++ src/components/Cards/PaymentCard.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+
+export default function PaymentCard({number, name, expDate}) {
+  return (
+    <div className= "w-[380px] h-[230px] bg-gradient-to-tr rounded-2xl flex content-center items-center justify-center from-[#9C2CF3] to-[#3A49F9]">
+
+    <div className="p-8 w-full h-full">
+    <div className="relative w-full h-full">
+      <Image
+        className="absolute"
+        alt=""
+        src="/mastercard.svg"
+        width={60}
+        height={24}
+      />
+      <Image
+        className="absolute right-0 bottom-0 top-0 my-auto"
+        alt=""
+        src="/chip.svg"
+        width={50}
+        height={30}
+      />
+      <div className="flex flex-col w-full h-full justify-end gap-4">
+        <p className="text-2xl">{number} </p>
+        <div className="flex gap-28">
+          <p className="text-lg uppercase">{name} </p>
+          <p className="text-lg uppercase">{expDate} </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  )
+}
