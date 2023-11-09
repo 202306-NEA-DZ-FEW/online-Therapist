29003482914e2e60b6edef7228ebb4e7179e62ac
Lilia <liliabendjeddou9@gmail.com>
2023-11-09 20:05:10 +0100

    build(api): fix bug

--- src/components/Cards/TicketCard.jsx
+++ src/components/Cards/TicketCard.jsx
@@ -4,9 +4,7 @@ import { useRouter } from "next/router";
 
 const TicketCard = ({ price }) => {
     const router = useRouter();
-    const {
-        user
-    } = UserAuth();
+    const { user } = UserAuth();
     const handleClick = () => {
         // Check if the user is logged in
         if (!user) {
@@ -15,7 +13,7 @@ const TicketCard = ({ price }) => {
         } else {
             router.push(`/buyTicket/${price.id}`);
         }
-    }
+    };
 
     return (
         <div className='bg-white/95 flex flex-col items-center justify-center text-center py-16 rounded-2xl space-y-6 shadow-2xl md:last-of-type:w-full md:last-of-type:mx-auto md:last-of-type:col-span-2 lg:last-of-type:col-auto lg:last-of-type:w-full'>
@@ -28,9 +26,12 @@ const TicketCard = ({ price }) => {
                 {price.unit_amount / 100}$
             </span>
             <div className='w-fit'>
-                    <button onClick={handleClick} className='uppercase cursor-pointer py-2 px-3 text-lg rounded text-black/80 font-light bg-[#2DD3E3]'>
-                        purchase
-                    </button>
+                <button
+                    onClick={handleClick}
+                    className='uppercase cursor-pointer py-2 px-3 text-lg rounded text-black/80 font-light bg-[#2DD3E3]'
+                >
+                    purchase
+                </button>
             </div>
         </div>
     );
