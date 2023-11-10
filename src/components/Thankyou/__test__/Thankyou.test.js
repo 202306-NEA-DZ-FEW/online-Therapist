807af9f14b07618bcdfe1fc6b29e2d035158c681
HabchiSidAli <sidalihabchi96@gmail.com>
2023-11-10 21:18:11 +0100

    feat(addnewcard): create add new credit card page
    create responsive page for users to add new credit cards
    
    #31

--- src/components/Thankyou/__test__/Thankyou.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from "react-test-renderer";
-import SignupThanks from "../SignupThanks";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<SignupThanks />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
