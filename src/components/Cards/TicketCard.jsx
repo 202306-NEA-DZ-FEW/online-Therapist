a3b6603c0ff2190895e123ee020f33852a898e54
Lilia <liliabendjeddou9@gmail.com>
2023-11-11 20:29:17 +0100

    feat(home page): enhance tickets section design

--- src/components/Cards/TicketCard.jsx
+++ src/components/Cards/TicketCard.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { UserAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
+import Button from "../elements/Button";
+import {GiCheckMark} from "react-icons/gi"
+import {TiTicket} from "react-icons/ti"
 
 const TicketCard = ({ price }) => {
     const router = useRouter();
@@ -14,24 +17,37 @@ const TicketCard = ({ price }) => {
             router.push(`/buyTicket/${price.id}`);
         }
     };
+    const ticketNumbers = Number(price.nickname)
 
     return (
-        <div className='bg-white/95 flex flex-col items-center justify-center text-center py-16 rounded-2xl space-y-6 shadow-2xl md:last-of-type:w-full md:last-of-type:mx-auto md:last-of-type:col-span-2 lg:last-of-type:col-auto lg:last-of-type:w-full'>
+        <div className='bg-white/95 flex flex-col items-center justify-center text-center py-10 rounded-2xl space-y-6 shadow-2xl md:last-of-type:w-full md:last-of-type:mx-auto md:last-of-type:col-span-2 lg:last-of-type:col-auto lg:last-of-type:w-full'>
+             <span className="text-4xl"><TiTicket/></span>
             <div className='flex items-center justify-center space-x-2'>
-                <span className='uppercase text-4xl'>
-                    {price.nickname} {"ticket"}
+                <span className='uppercase font-atkinson text-4xl'>
+                 {price.nickname} {"Tickets "} 
                 </span>
+                
             </div>
-            <span className='text-base rtl:text-3xl text-black/50'>
+            <span className='text-3xl rtl:text-3xl text-DarkTeal'>
                 {price.unit_amount / 100}$
             </span>
+            <p className='text-2xl text-DarkTeal'>{(price.unit_amount / 100)/ticketNumbers}$<span className="text-black">/ Ticket </span></p>
+            <div className="font-atkinson text-xl text-start">
+                <p>You can use the tickets for :</p>
+                <ul>
+                    <li className="flex flex-row space-x-2 items-center"><GiCheckMark/>  <span>Video calls</span></li>
+                    <li className="flex flex-row space-x-2 items-center"><GiCheckMark/>  <span>Chat</span></li>
+                    <li className="flex flex-row space-x-2 items-center"><GiCheckMark/>  <span>Voice calls</span></li>
+                </ul>
+            </div>
             <div className='w-fit'>
-                <button
-                    onClick={handleClick}
-                    className='uppercase cursor-pointer py-2 px-3 text-lg rounded text-black/80 font-light bg-[#2DD3E3]'
-                >
-                    purchase
-                </button>
+                <Button
+                color='teal'
+                transition={false}
+                buttonText='PURCHASE'
+                buttonSize='fit'
+                clickFunction={handleClick}
+                />
             </div>
         </div>
     );
