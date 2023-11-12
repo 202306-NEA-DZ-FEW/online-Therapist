089e69dd5148906dbe384f2f09c23178c36b11b9
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-12 02:24:41 +0100

    feat(therapists/profile): update profile informations on form submit
    update profile informations on form submit
    
    #20

--- src/pages/therapists/profile/index.jsx
+++ src/pages/therapists/profile/index.jsx
@@ -1,82 +1,79 @@
-import {yupResolver} from "@hookform/resolvers/yup"
-import {doc, getDoc, updateDoc} from "firebase/firestore"
-import {useSearchParams} from "next/navigation"
-import {useRouter} from "next/router"
-import {withTranslation} from "next-i18next"
-import {serverSideTranslations} from "next-i18next/serverSideTranslations"
-import {useState} from "react"
-import {useEffect} from "react"
-import {useForm} from "react-hook-form"
-import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { withTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
-import Button from "@/components/elements/Button"
-import Input from "@/components/elements/Input"
+import Button from "@/components/elements/Button";
+import Input from "@/components/elements/Input";
 
-import {useAuth} from "@/context/AuthContext"
-import Layout from "@/layout/Layout"
-import {db} from "@/util/firebase"
-const Profile = ({t}) => {
-    const router = useRouter()
-    const searchParams = useSearchParams()
+import { useAuth } from "@/context/AuthContext";
+import Layout from "@/layout/Layout";
+import { db } from "@/util/firebase";
+const Profile = ({ t }) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
 
     /** state */
     const [edit, setEdit] = useState(
         searchParams.get("edit") == "true" ? true : false
-    )
-    const [formErrors, setFormErrors] = useState("")
-    const {user} = useAuth()
-    const [formData, setFormData] = useState({
-
-    })
-
+    );
+    const [formErrors, setFormErrors] = useState("");
+    const { user } = useAuth();
+    const [formData, setFormData] = useState({});
 
     const onChange = (e) => {
-        const {name, value} = e.target
-        setFormData({...formData, [name]: value})
-    }
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
     const validationSchema = yup.object().shape({
         age: yup.string().required(t("therapists:formErrors.username")),
         gender: yup.string().oneOf(["male", "female"]),
-        speciality: yup.string().oneOf(["teen counceling", "couple counceling", "individual counceling"]),
-
-    })
-    const formOptions = {resolver: yupResolver(validationSchema)}
-    const {register, handleSubmit, formState} = useForm(formOptions)
-    const {errors} = formState
+        speciality: yup
+            .string()
+            .oneOf([
+                "teen counceling",
+                "couple counceling",
+                "individual counceling",
+            ]),
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
+    const { register, handleSubmit, formState } = useForm(formOptions);
+    const { errors } = formState;
     const enableEdit = (e) => {
-        e.preventDefault()
-        setEdit((edit) => !edit)
-        router.query.edit = true
-        router.push(router)
-
-    }
+        e.preventDefault();
+        setEdit((edit) => !edit);
+        router.query.edit = true;
+        router.push(router);
+    };
     const onSubmit = async () => {
         try {
-            const therapistRef = doc(db, "therapists", user.uid)
-            await updateDoc(therapistRef, {...formData})
-
+            const therapistRef = doc(db, "therapists", user.uid);
+            await updateDoc(therapistRef, { ...formData });
         } catch (err) {
-            setFormErrors({...formErrors, err})
+            setFormErrors({ ...formErrors, err });
         }
-    }
-    async function fetchTherapist () {
-        const docRef = doc(db, "therapists", user.uid)
-        const docSnap = await getDoc(docRef)
+    };
+    async function fetchTherapist() {
+        const docRef = doc(db, "therapists", user.uid);
+        const docSnap = await getDoc(docRef);
 
-        setFormData(docSnap.data())
+        setFormData(docSnap.data());
     }
-
 
     useEffect(() => {
-        fetchTherapist()
-    }, [])
+        fetchTherapist();
+    }, []);
 
     return (
         <Layout>
             <div className='container mx-auto font-atkinson '>
-
                 {(user?.isTherapist && (
-
                     <div className='grid grid-cols-1  lg:grid-cols-2 py-20 gap-y-10'>
                         <div className='justify-self-center lg:justify-self-start'>
                             Photo component
@@ -84,24 +81,22 @@ const Profile = ({t}) => {
 
                         <form
                             onSubmit={handleSubmit(onSubmit)}
-                            className='w-full justify-self-center lg:justify-self-end'>
+                            className='w-full justify-self-center lg:justify-self-end'
+                        >
                             <legend class='text-3xl md:text-4xl text-center md:text-start font-medium uppercase'>
                                 {t("therapist profile")}
                             </legend>
                             <div className=' lg:mx-0 mx-2 p-1 flex items-center my-5 gap-2 min-w-max '>
                                 <Input
                                     label='Full Name'
-                                    name="fullname"
+                                    name='fullname'
                                     styles='md:ml-[6.5rem] lg:w-[26.9rem] md:w-[34rem] rtl:md:mr-[6.5rem] rtl:ml-0 rtl:md:w-[28.4rem] rtl:lg:w-[21.5rem]'
                                     isDisabled={!edit}
                                     placeholder=''
-                                    errorMessage={
-                                        errors.fullname?.message
-                                    }
-                                    register={{...register("fullname")}}
+                                    errorMessage={errors.fullname?.message}
+                                    register={{ ...register("fullname") }}
                                     value={formData.fullname}
                                     onChange={onChange}
-
                                 />
                             </div>
                             <div className='lg:mx-0 mx-2 p-1 flex flex-col md:flex-row items-start my-5 gap-2'>
@@ -116,28 +111,23 @@ const Profile = ({t}) => {
                                     placeholder=''
                                     disabled={!edit}
                                     name='bio'
-
-                                    register={{...register("bio")}}
+                                    register={{ ...register("bio") }}
                                     value={formData.bio}
                                     onChange={onChange}
                                 ></textarea>
                                 <p className='text-sm text-red-500 mt-1 animate-pulse '>
-                                    {
-                                        errors.bio?.message
-                                    }
+                                    {errors.bio?.message}
                                 </p>
                             </div>
                             <div className=' lg:mx-0 mx-2 p-1 flex items-center my-5 gap-2'>
                                 <Input
                                     label='Birth Date'
-                                    name="birthdate"
+                                    name='birthdate'
                                     type='date'
                                     styles='md:ml-[6.4rem] rtl:md:mr-[6.4rem] rtl:ml-[0rem] rtl:w-[21.5rem] md:w-[34rem] lg:w-[27rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem]'
                                     isDisabled={!edit}
-                                    errorMessage={
-                                        errors.birthdate?.message
-                                    }
-                                    register={{...register("birthdate")}}
+                                    errorMessage={errors.birthdate?.message}
+                                    register={{ ...register("birthdate") }}
                                     value={formData.birthdate}
                                     onChange={onChange}
                                 />
@@ -146,14 +136,12 @@ const Profile = ({t}) => {
                                 <Input
                                     label='Email'
                                     type='email'
-                                    name="email"
+                                    name='email'
                                     styles='md:ml-[8.9rem] rtl:md:mr-[8.9rem] rtl:w-full rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] mx-0 md:w-[34.1rem] lg:w-[27rem]'
                                     isDisabled={!edit}
                                     placeholder=''
-                                    errorMessage={
-                                        errors.email?.message
-                                    }
-                                    register={{...register("email")}}
+                                    errorMessage={errors.email?.message}
+                                    register={{ ...register("email") }}
                                     value={formData.email}
                                     onChange={onChange}
                                 />
@@ -162,38 +150,33 @@ const Profile = ({t}) => {
                                 <Input
                                     label='Phone Numer'
                                     type='tel'
-                                    name="phone"
+                                    name='phone'
                                     styles='md:ml-[4.5rem] rtl:md:mr-[4.5rem] rtl:w-full
                                     rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] md:w-[34rem] lg:w-[27rem]'
                                     isDisabled={!edit}
                                     placeholder=''
-                                    errorMessage={
-                                        errors.phone?.message
-                                    }
-                                    register={{...register("phone")}}
+                                    errorMessage={errors.phone?.message}
+                                    register={{ ...register("phone") }}
                                     value={formData.phone}
                                     onChange={onChange}
                                 />
                             </div>
                             <div className='lg:mx-0 mx-2 p-1 flex items-center my-5 gap-2 min-w-max '>
-                                <div className="flex flex-col w-full">
+                                <div className='flex flex-col w-full'>
                                     <Input
                                         label='Age'
                                         type='number'
-                                        name="age"
+                                        name='age'
                                         styles='md:ml-[9.8rem] rtl:md:ml-[0rem] rtl:md:mr-[9.8rem] rtl:w-full
                                     rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] md:w-[34rem] lg:w-[27rem]'
                                         isDisabled={!edit}
                                         placeholder=''
-
-                                        register={{...register("age")}}
+                                        register={{ ...register("age") }}
                                         value={formData.age}
                                         onChange={onChange}
                                     />
                                     <p className='md:self-center md:ml-[-14rem] lg:ml-[-7rem] text-sm text-red-500 mt-1 animate-pulse '>
-                                        {
-                                            errors.age?.message
-                                        }
+                                        {errors.age?.message}
                                     </p>
                                 </div>
                             </div>
@@ -205,30 +188,28 @@ const Profile = ({t}) => {
                                 >
                                     Gender
                                 </label>
-                                <div className="w-full mx-0 flex flex-col md:w-[35rem] lg:w-[27.5rem]">
+                                <div className='w-full mx-0 flex flex-col md:w-[35rem] lg:w-[27.5rem]'>
                                     <select
                                         disabled={!edit}
                                         name='gender'
                                         required
                                         className='md:w-[34rem] lg:w-[27rem] w-full  md:ml-[7.4rem] rtl:md:mr-[7.5rem] rtl:md:min-w-[21.5rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] border border-gray-300 h-12 bg-white pl-4 rounded-md p-2 focus:outline-none focus:border-Teal focus:ring-Teal invalid:border-red-500 invalid:text-red-500 peer'
-
-                                        register={{...register("gender")}}
+                                        register={{ ...register("gender") }}
                                         value={formData.gender}
                                         onChange={onChange}
                                     >
-                                        <option selected disabled> Choose ...</option>
+                                        <option selected disabled>
+                                            {" "}
+                                            Choose ...
+                                        </option>
                                         <option value='male'>Male</option>
                                         <option value='female'>Female</option>
-
                                     </select>
                                     <p className='md:self-center md:ml-[-12rem] lg:ml-[-4rem] text-sm text-red-500 mt-1 animate-pulse '>
-                                        {
-                                            errors.gender?.message
-                                        }
+                                        {errors.gender?.message}
                                     </p>
                                 </div>
                             </div>
-
 
                             <div className=' lg:mx-0 mx-2 p-1 flex flex-col md:flex-row my-5 gap-2 min-w-max'>
                                 <label
@@ -237,14 +218,12 @@ const Profile = ({t}) => {
                                 >
                                     Speciality
                                 </label>
-                                <div className="w-full mx-0 flex flex-col md:w-[35rem] lg:w-[27.5rem] ">
-
+                                <div className='w-full mx-0 flex flex-col md:w-[35rem] lg:w-[27.5rem] '>
                                     <select
                                         disabled={!edit}
                                         name='speciality'
                                         className='md:w-[34rem] lg:w-[27rem] w-full md:ml-[6.3rem]  rtl:md:mr-[6.3rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] border border-gray-300 h-12 bg-white pl-4 rounded-md p-2 focus:outline-none focus:border-Teal focus:ring-Teal invalid:border-red-500 invalid:text-red-500 peer'
-
-                                        register={{...register("speciality")}}
+                                        register={{ ...register("speciality") }}
                                         value={formData.speciality}
                                         onChange={onChange}
                                     >
@@ -258,23 +237,20 @@ const Profile = ({t}) => {
                                         <option value='teen counceling'>
                                             Teen Counceling
                                         </option>
-
                                     </select>
                                     <p className='md:self-center md:ml-[-16rem] lg:ml-[-8rem]  text-sm text-red-500 mt-1 animate-pulse '>
-                                        {
-                                            errors.speciality?.message
-                                        }
+                                        {errors.speciality?.message}
                                     </p>
                                 </div>
                             </div>
                             <div className='flex flex-row justify-center gap-5 my-14 lg:ml-8 lg:rtl:mr-20'>
-                                <button type="submit">
+                                <button type='submit'>
                                     <Button
                                         buttonText='Save'
                                         disabled={!edit}
                                         transition={false}
                                         color='teal'
-                                        type="submit"
+                                        type='submit'
                                     />
                                 </button>
                                 <Button
@@ -290,16 +266,16 @@ const Profile = ({t}) => {
                 )) || <div></div>}
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default withTranslation("therapists")(Profile)
+export default withTranslation("therapists")(Profile);
 
-export async function getStaticProps ({locale}) {
+export async function getStaticProps({ locale }) {
     return {
         props: {
             ...(await serverSideTranslations(locale, ["common", "therapists"])),
             // Will be passed to the page component as props.
         },
-    }
+    };
 }
