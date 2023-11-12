da70a014cd6dae1254a0bb5c9f8738dd148dae41
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-12 20:57:43 +0100

    feat(therapists/profile): change default profile image and make form to be controlled
    change default profile image and make form to be controlled
    
    #20

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,41 +1,41 @@
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage"
-import Image from "next/image"
-import Edit from "public/edit-profile.svg"
-import Profile from "public/profile.png"
-import Spinner from "public/spinner.svg"
-import {useState} from "react"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Image from "next/image";
+import Edit from "public/edit-profile.svg";
+import Profile from "public/profile.png";
+import Spinner from "public/spinner.svg";
+import { useState } from "react";
 
-import {useAuth} from "@/context/AuthContext"
-import {storage} from "@/util/firebase"
+import { useAuth } from "@/context/AuthContext";
+import { storage } from "@/util/firebase";
 const ProfileImage = () => {
-    const [uploading, setUploading] = useState(false)
-    const {user, updateProfilePhoto} = useAuth()
-    const [ProfileImage, setProfileImage] = useState("")
+    const [uploading, setUploading] = useState(false);
+    const { user, updateProfilePhoto } = useAuth();
+    const [ProfileImage, setProfileImage] = useState("");
     const handleUpload = async (e) => {
-        const file = e.target.files[0]
-        const imageName = `${user.uid}`
-        const path = "images/users/"
-        setProfileImage(URL.createObjectURL(file))
+        const file = e.target.files[0];
+        const imageName = `${user.uid}`;
+        const path = "images/users/";
+        setProfileImage(URL.createObjectURL(file));
         try {
-            if (!file) return
-            const imageRef = ref(storage, `${path}/${imageName}`)
+            if (!file) return;
+            const imageRef = ref(storage, `${path}/${imageName}`);
             try {
-                await uploadBytes(imageRef, file)
+                await uploadBytes(imageRef, file);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-            setUploading(true)
-            const profileImageRef = ref(storage, `${path}${imageName}`)
-            const downloadURL = await getDownloadURL(profileImageRef)
-            updateProfilePhoto(downloadURL)
-            setProfileImage(downloadURL)
-            localStorage.setItem(`therapist_image_${user.uid}`, downloadURL)
+            setUploading(true);
+            const profileImageRef = ref(storage, `${path}${imageName}`);
+            const downloadURL = await getDownloadURL(profileImageRef);
+            updateProfilePhoto(downloadURL);
+            setProfileImage(downloadURL);
+            localStorage.setItem(`therapist_image_${user.uid}`, downloadURL);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         } finally {
-            setUploading(false)
+            setUploading(false);
         }
-    }
+    };
     return (
         <>
             <div className='relative inline-flex justify-center w-[14rem] h-[14rem] lg:w-[20rem] lg:h-[20rem] xl:w-[18rem] xl:h-[18rem] rounded-full border-2 border-black '>
@@ -43,10 +43,12 @@ const ProfileImage = () => {
                     src={
                         uploading
                             ? Spinner
-                            : localStorage.getItem(`therapist_image_${user.uid}`) ||
-                            user.photoURL ||
-                            ProfileImage ||
-                            Profile
+                            : localStorage.getItem(
+                                  `therapist_image_${user.uid}`
+                              ) ||
+                              user.photoURL ||
+                              ProfileImage ||
+                              Profile
                     }
                     alt='profile preview'
                     width={400}
@@ -67,7 +69,7 @@ const ProfileImage = () => {
                 </label>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default ProfileImage
+export default ProfileImage;
