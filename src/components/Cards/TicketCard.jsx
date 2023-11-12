3dbc87119c06ad3707479340028ab0ec86d2fbe0
Lilia <liliabendjeddou9@gmail.com>
2023-11-12 15:16:24 +0100

    feat(ticket card): change the icons

--- src/components/Cards/TicketCard.jsx
+++ src/components/Cards/TicketCard.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import Button from "../elements/Button";
 import { GiCheckMark } from "react-icons/gi";
 import { TiTicket } from "react-icons/ti";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const TicketCard = ({ price }) => {
     const router = useRouter();
@@ -21,32 +22,32 @@ const TicketCard = ({ price }) => {
 
     return (
         <div className='bg-white/95 flex flex-col items-center justify-center text-center py-10 rounded-2xl space-y-6 shadow-2xl md:last-of-type:w-full md:last-of-type:mx-auto md:last-of-type:col-span-2 lg:last-of-type:col-auto lg:last-of-type:w-full'>
-            <span className='text-4xl'>
-                <TiTicket />
+            <span className='flex text-4xl'>
+            <TiTicket />
             </span>
             <div className='flex items-center justify-center space-x-2'>
                 <span className='uppercase font-atkinson text-4xl'>
                     {price.nickname} {"Tickets "}
                 </span>
             </div>
-            <span className='text-3xl rtl:text-3xl text-DarkTeal'>
+            <span className='text-3xl rtl:text-3xl font-semibold text-Teal'>
                 {price.unit_amount / 100}$
             </span>
-            <p className='text-2xl text-DarkTeal'>
-                {price.unit_amount / 100 / ticketNumbers}$
-                <span className='text-black'>/ Ticket </span>
+            <p>
+                 <span className='text-2xl font-semibold text-Teal'>{price.unit_amount / 100 / ticketNumbers}$</span>
+                <span className='text-black text-lg'>/ Ticket </span>
             </p>
             <div className='font-atkinson text-xl text-start'>
                 <p>You can use the tickets for :</p>
                 <ul>
                     <li className='flex flex-row space-x-2 items-center'>
-                        <GiCheckMark /> <span>Video calls</span>
+                        <BsCheckCircleFill className="text-Teal"/> <span>Video calls</span>
                     </li>
                     <li className='flex flex-row space-x-2 items-center'>
-                        <GiCheckMark /> <span>Chat</span>
+                        <BsCheckCircleFill className="text-Teal"/> <span>Chat</span>
                     </li>
                     <li className='flex flex-row space-x-2 items-center'>
-                        <GiCheckMark /> <span>Voice calls</span>
+                        <BsCheckCircleFill className="text-Teal" /> <span>Voice calls</span>
                     </li>
                 </ul>
             </div>
--- src/components/Home/AreYouCouncelor.jsx
+++ src/components/Home/AreYouCouncelor.jsx
@@ -12,31 +12,34 @@ const AreYouCouncelor = () => {
                 Are you a counselor?
             </p>
             <div className='bg-white/95 flex flex-col lg:flex-row items-center justify-center text-start  rounded-md  shadow-2xl min-h-[20rem]'>
-               <div className="p-16 space-y-4 lg:p-0 lg:space-y-0 lg:flex lg:flex-col lg:items-center lg:justify-center">
-
-                <span className='text-lg p-12 font-atkinson text-black/80'>
-                    Interested in becoming a part of our revolutionary mental
-                    health platform? We offer you the freedom to set your own
-                    schedule and decide how much you want to work. Meanwhile, we
-                    take care of all the heavy lifting, from handling client
-                    referrals to managing all billing details. Join us and focus
-                    on what you do best – providing quality mental health
-                    services, while we ensure a seamless experience for both you
-                    and your clients.
-                </span>
-                <div className="flex items-center justify-center ">
-                    <Link href='/requirements'>
-                        <Button
-                                        color='teal'
-                                        transition={false}
-                                        buttonText='LEARN MORE'
-                                        buttonSize='lg'
-                                        className="xl"
-                                    />
-                    </Link>
+                <div className='p-16 space-y-4 lg:p-0 lg:space-y-0 lg:flex lg:flex-col lg:items-center lg:justify-center'>
+                    <span className='text-lg p-12 font-atkinson text-black/80'>
+                        Interested in becoming a part of our revolutionary
+                        mental health platform? We offer you the freedom to set
+                        your own schedule and decide how much you want to work.
+                        Meanwhile, we take care of all the heavy lifting, from
+                        handling client referrals to managing all billing
+                        details. Join us and focus on what you do best –
+                        providing quality mental health services, while we
+                        ensure a seamless experience for both you and your
+                        clients.
+                    </span>
+                    <div className='flex items-center justify-center '>
+                        <Link href='/requirements'>
+                            <Button
+                                color='teal'
+                                transition={false}
+                                buttonText='LEARN MORE'
+                                buttonSize='lg'
+                                className='xl'
+                            />
+                        </Link>
+                    </div>
                 </div>
-               </div>
-            <img className="w-96" src="https://img.freepik.com/photos-gratuite/therapeute-femme-mid-shot-presse-papiers_23-2148759113.jpg?w=740&t=st=1699732213~exp=1699732813~hmac=35c0aab09ef3427110b056edfd168654a1c1643ba81298c3268a4b9d7a06805d"/>
+                <img
+                    className='w-96'
+                    src='https://img.freepik.com/photos-gratuite/therapeute-femme-mid-shot-presse-papiers_23-2148759113.jpg?w=740&t=st=1699732213~exp=1699732813~hmac=35c0aab09ef3427110b056edfd168654a1c1643ba81298c3268a4b9d7a06805d'
+                />
             </div>
         </div>
     );
