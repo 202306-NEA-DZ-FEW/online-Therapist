7d4df8272bd35689234f48f43c9f36a97c5417f3
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-12 03:47:15 +0100

    feat(therapists/profile): add ProfileImage component and handle upload to firebase storage
    add ProfileImage component and handle upload to firebase storage
    
    #20

--- src/pages/therapists/profile/index.jsx
+++ src/pages/therapists/profile/index.jsx
@@ -1,64 +1,65 @@
-import {doc, getDoc, updateDoc} from "firebase/firestore"
-import {useSearchParams} from "next/navigation"
-import {useRouter} from "next/router"
-import {withTranslation} from "next-i18next"
-import {serverSideTranslations} from "next-i18next/serverSideTranslations"
-import {useState} from "react"
-import {useEffect} from "react"
-import {useForm} from "react-hook-form"
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { withTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-import Button from "@/components/elements/Button"
-import Input from "@/components/elements/Input"
-import ProfileImage from "@/components/ProfileImage"
+import Button from "@/components/elements/Button";
+import Input from "@/components/elements/Input";
+import ProfileImage from "@/components/ProfileImage";
 
-import {useAuth} from "@/context/AuthContext"
-import Layout from "@/layout/Layout"
-import {db} from "@/util/firebase"
-const Profile = ({t}) => {
-    const router = useRouter()
-    const searchParams = useSearchParams()
+import { useAuth } from "@/context/AuthContext";
+import Layout from "@/layout/Layout";
+import { db } from "@/util/firebase";
+const Profile = ({ t }) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
 
     /** state */
     const [edit, setEdit] = useState(
         searchParams.get("edit") == "true" ? true : false
-    )
-    const [formErrors, setFormErrors] = useState("")
-    const [photo, setPhoto] = useState(localStorage?.getItem("therapist_image"))
-    const {user} = useAuth()
-    const [formData, setFormData] = useState({})
+    );
+    const [formErrors, setFormErrors] = useState("");
+    const [photo, setPhoto] = useState(
+        localStorage?.getItem("therapist_image")
+    );
+    const { user } = useAuth();
+    const [formData, setFormData] = useState({});
 
     const onChange = (e) => {
-        const {name, value} = e.target
-        setFormData({...formData, [name]: value})
-    }
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
-
-    const {register, handleSubmit, formState} = useForm()
-    const {errors} = formState
+    const { register, handleSubmit, formState } = useForm();
+    const { errors } = formState;
     const enableEdit = (e) => {
-        e.preventDefault()
-        setEdit((edit) => !edit)
-        router.query.edit = true
-        router.push(router)
-    }
+        e.preventDefault();
+        setEdit((edit) => !edit);
+        router.query.edit = true;
+        router.push(router);
+    };
     const onSubmit = async () => {
         try {
-            const therapistRef = doc(db, "therapists", user.uid)
-            await updateDoc(therapistRef, {...formData, photoURL: photo})
+            const therapistRef = doc(db, "therapists", user.uid);
+            await updateDoc(therapistRef, { ...formData, photoURL: photo });
         } catch (err) {
-            setFormErrors({...formErrors, err})
+            setFormErrors({ ...formErrors, err });
         }
-    }
-    async function fetchTherapist () {
-        const docRef = doc(db, "therapists", user.uid)
-        const docSnap = await getDoc(docRef)
+    };
+    async function fetchTherapist() {
+        const docRef = doc(db, "therapists", user.uid);
+        const docSnap = await getDoc(docRef);
 
-        setFormData({...docSnap.data()})
+        setFormData({ ...docSnap.data() });
     }
 
     useEffect(() => {
-        fetchTherapist()
-    }, [])
+        fetchTherapist();
+    }, []);
 
     return (
         <Layout>
@@ -84,7 +85,7 @@ const Profile = ({t}) => {
                                     isDisabled={!edit}
                                     placeholder=''
                                     errorMessage={errors.fullname?.message}
-                                    register={{...register("fullname")}}
+                                    register={{ ...register("fullname") }}
                                     value={formData.fullname}
                                     onChange={onChange}
                                 />
@@ -101,7 +102,7 @@ const Profile = ({t}) => {
                                     placeholder=''
                                     disabled={!edit}
                                     name='bio'
-                                    register={{...register("bio")}}
+                                    register={{ ...register("bio") }}
                                     value={formData.bio}
                                     onChange={onChange}
                                 ></textarea>
@@ -117,7 +118,7 @@ const Profile = ({t}) => {
                                     styles='md:ml-[6.4rem] rtl:md:mr-[6.4rem] rtl:ml-[0rem] rtl:w-[27rem] md:w-[34rem] lg:w-[27rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem]'
                                     isDisabled={!edit}
                                     errorMessage={errors.birthdate?.message}
-                                    register={{...register("birthdate")}}
+                                    register={{ ...register("birthdate") }}
                                     value={formData.birthdate}
                                     onChange={onChange}
                                 />
@@ -131,7 +132,7 @@ const Profile = ({t}) => {
                                     isDisabled={!edit}
                                     placeholder=''
                                     errorMessage={errors.email?.message}
-                                    register={{...register("email")}}
+                                    register={{ ...register("email") }}
                                     value={formData.email}
                                     onChange={onChange}
                                 />
@@ -146,7 +147,7 @@ const Profile = ({t}) => {
                                     isDisabled={!edit}
                                     placeholder=''
                                     errorMessage={errors.phone?.message}
-                                    register={{...register("phone")}}
+                                    register={{ ...register("phone") }}
                                     value={formData.phone}
                                     onChange={onChange}
                                 />
@@ -161,7 +162,7 @@ const Profile = ({t}) => {
                                     rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] md:w-[34rem] lg:w-[27rem]'
                                         isDisabled={!edit}
                                         placeholder=''
-                                        register={{...register("age")}}
+                                        register={{ ...register("age") }}
                                         value={formData.age}
                                         onChange={onChange}
                                     />
@@ -184,7 +185,7 @@ const Profile = ({t}) => {
                                         name='gender'
                                         required
                                         className='md:w-[34rem] lg:w-[27rem] w-full  md:ml-[7.4rem] rtl:md:mr-[7.5rem] rtl:md:min-w-[21.5rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] border border-gray-300 h-12 bg-white pl-4 rounded-md p-2 focus:outline-none focus:border-Teal focus:ring-Teal invalid:border-red-500 invalid:text-red-500 peer cursor-pointer'
-                                        register={{...register("gender")}}
+                                        register={{ ...register("gender") }}
                                         value={formData.gender}
                                         onChange={onChange}
                                     >
@@ -213,7 +214,7 @@ const Profile = ({t}) => {
                                         disabled={!edit}
                                         name='speciality'
                                         className='md:w-[34rem] lg:w-[27rem] w-full md:ml-[6.3rem]  rtl:md:mr-[6.3rem] rtl:md:w-[28.5rem] rtl:lg:w-[21.5rem] border border-gray-300 h-12 bg-white pl-4 rounded-md p-2 focus:outline-none focus:border-Teal focus:ring-Teal invalid:border-red-500 invalid:text-red-500 peer cursor-pointer'
-                                        register={{...register("speciality")}}
+                                        register={{ ...register("speciality") }}
                                         value={formData.speciality}
                                         onChange={onChange}
                                     >
@@ -256,16 +257,16 @@ const Profile = ({t}) => {
                 )) || <div></div>}
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default withTranslation("therapists")(Profile)
+export default withTranslation("therapists")(Profile);
 
-export async function getStaticProps ({locale}) {
+export async function getStaticProps({ locale }) {
     return {
         props: {
             ...(await serverSideTranslations(locale, ["common", "therapists"])),
             // Will be passed to the page component as props.
         },
-    }
+    };
 }
