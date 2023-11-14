59df1e1efa4695ded3aa2c644c3166de1d34d100
Lilia <liliabendjeddou9@gmail.com>
2023-11-14 19:27:58 +0100

    style(layout): add animation

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import { motion } from "framer-motion";
 
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
+            <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 15 }}
+        transition={{ delay: 0.25 }}
+      >
+
             {children}
+      </motion.div>
             <Footer />
         </>
     );
