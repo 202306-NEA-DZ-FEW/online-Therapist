230c5050062fc6f13a7b898806b008c51f1f347a
Lilia <liliabendjeddou9@gmail.com>
2023-11-14 19:42:30 +0100

    style(layout): add more animation

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,14 +8,13 @@ export default function Layout({ children }) {
         <>
             <Navbar />
             <motion.div
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 15 }}
-        transition={{ delay: 0.25 }}
-      >
-
-            {children}
-      </motion.div>
+                initial={{ opacity: 0, y: 15 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 15 }}
+                transition={{ delay: 0.25 }}
+            >
+                {children}
+            </motion.div>
             <Footer />
         </>
     );
