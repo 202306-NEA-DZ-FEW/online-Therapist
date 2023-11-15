738dcc7839d2de74bc9c5295e7a91043953753f1
Lilia <liliabendjeddou9@gmail.com>
2023-11-15 21:00:34 +0100

    fix(prettier): fix prettier issue

--- src/components/Navbar/dropdowns/UserDropdown.jsx
+++ src/components/Navbar/dropdowns/UserDropdown.jsx
@@ -55,9 +55,7 @@ export default function UserDropdown() {
             <hr />
             <ul className='py-2 first:pt-0 last:pb-0'>
                 <Link href='/userProfile/'>
-                    <li
-                        className='flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'
-                    >
+                    <li className='flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'>
                         <IoSettingsOutline />
                         {t("navbar.account")}
                     </li>
