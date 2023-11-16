8c610280c3eb555102189ee8ce32e3de61ae1f47
Lilia <liliabendjeddou9@gmail.com>
2023-11-16 20:07:13 +0100

    feat(therapist dashboard): creat therapist dashboard

--- src/util/library.js
+++ src/util/library.js
@@ -1,3 +1,11 @@
+import { HiUserGroup } from "react-icons/hi";
+import { LuMessagesSquare } from "react-icons/lu";
+import { IoCalendar } from "react-icons/io5";
+import { FaClipboardList } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { HiHome } from "react-icons/hi";
+
 export const images = [
     {
         id: 1,
@@ -66,5 +74,57 @@ export const teamMembers = [
         name: "Jor-El Caraballo",
         //   linkedin: "",
         image: "https://www.talkspace.com/blog/wp-content/uploads/2020/06/Jor-El-Caraballo-150x150.jpg",
+    },
+];
+
+export  const links = [
+    {
+        id: 1,
+        name: "home",
+        href: "/",
+        icon: <HiHome />,
+        text: "Home",
+    },
+    {
+        id: 2,
+        name: "appointments",
+        href: "",
+        icon: <FaClipboardList />,
+        text: "Appointments",
+    },
+    {
+        id: 3,
+        name: "messages",
+        href: "",
+        icon: <LuMessagesSquare />,
+        text: "Messages",
+    },
+    {
+        id: 4,
+        name: "calendar",
+        href: "",
+        icon: <IoCalendar />,
+        text: "Calendar",
+    },
+    {
+        id: 5,
+        name: "patients",
+        href: "",
+        icon: <HiUserGroup />,
+        text: "Patients",
+    },
+    {
+        id: 6,
+        name: "profile",
+        href: "/therapists/profile/",
+        icon: <CgProfile />,
+        text: "Profile",
+    },
+    {
+        id: 7,
+        name: "settings",
+        href: "/therapists/profile/",
+        icon: <IoSettingsSharp />,
+        text: "Settings",
     },
 ];
