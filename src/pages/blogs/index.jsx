cf637f2b753abe55c0ca38a5527c1a8be2d84401
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-21 23:30:52 +0100

    feat(blogs): fix grid system && minor styling changes
    fix grid system && minor styling changes
    
    #35

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,37 +1,37 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { groq } from "next-sanity";
-import React from "react";
+import Image from "next/image"
+import Link from "next/link"
+import {useTranslation} from "next-i18next"
+import {serverSideTranslations} from "next-i18next/serverSideTranslations"
+import {groq} from "next-sanity"
+import React from "react"
 import {
     PiArrowLineUpLeftLight,
     PiArrowLineUpRightLight,
-} from "react-icons/pi";
+} from "react-icons/pi"
 
-import Layout from "@/layout/Layout";
+import Layout from "@/layout/Layout"
 
-import { client } from "../../../sanity/lib/client";
-import { urlForImage } from "../../../sanity/lib/image";
+import {client} from "../../../sanity/lib/client"
+import {urlForImage} from "../../../sanity/lib/image"
 
-const Blogs = ({ posts }) => {
-    const { t } = useTranslation("blog");
+const Blogs = ({posts}) => {
+    const {t} = useTranslation("blog")
     return (
         <Layout>
             <div className='container mx-auto w-full font-atkinson  mb-10'>
-                <div className='grid grid-cols-1 md:grid-cols-2 mx-10 mt-5 '>
-                    <div className='shadow-md overflow-clip rounded-md mt-5 mx-3'>
+                <div className='mx-2 mt-5 '>
+                    <div className=' mt-5 mx-3 grid grid-cols-1 md:grid-cols-3 gap-5'>
                         {posts.map((post) => (
                             <Link
                                 key={post._id}
-                                href={`/${
-                                    post.language == "ar" ? "/ar" : ""
-                                }/blogs/${post.slug.current}`}
+                                href={`/${post.language == "ar" ? "/ar" : ""
+                                    }/blogs/${post.slug.current}`}
+                                className="overflow-clip shadow-lg rounded-md mt-5"
                             >
                                 <div className='flex flex-col group cursor-pointer '>
-                                    <div className='relative min-w-full will-change-contents h-56 md:h-80'>
+                                    <div className='relative min-w-full will-change-contents  md:h-64 h-56'>
                                         <Image
-                                            className='object-cover object-left rtl:object-right
+                                            className='object-cover 
             lg:object-center group-hover:scale-[1.07]  transition-all duration-300 ease-out '
                                             src={urlForImage(
                                                 post.mainImage
@@ -43,12 +43,12 @@ const Blogs = ({ posts }) => {
                                             className='grid grid-cols-1 md:grid-cols-2 absolute bottom-0 bg-opacity-30 w-full rounded  bg-black/20 
                       backdrop-blur-md text-white/90 p-7  justify-between group-hover:-bottom-3 group-hover:backdrop-blur-[5px] transition-all duration-300 ease-out '
                                         >
-                                            <div className='md:flex flex-col hidden'>
-                                                <p className='font-bold text-lg line-clamp-1'>
-                                                    {t("written_by")} :{" "}
+                                            <div className='md:flex flex-col hidden -ml-5 rtl:-mr-5'>
+                                                <p className='font-bold text-base w-56'>
+                                                    {t("written_by")} : {" "}
                                                     {post.author.name}
                                                 </p>
-                                                <p className='text-md'>
+                                                <p className='text-xs'>
                                                     {new Date(
                                                         post._createdAt
                                                     ).toLocaleDateString(
@@ -64,7 +64,7 @@ const Blogs = ({ posts }) => {
                                                 </p>
                                             </div>
 
-                                            <div className='flex flex-row items-center justify-start md:justify-end mt-2'>
+                                            <div className='flex flex-row items-center justify-start md:justify-end -mr-5 rtl:-ml-2 mt-2'>
                                                 {post.categories?.map(
                                                     (category) => (
                                                         <div
@@ -81,7 +81,7 @@ const Blogs = ({ posts }) => {
                                         </div>
                                     </div>
                                     <div className='mt-7 flex-1 px-7 py-2'>
-                                        <p className='underline underline-offset-[4px] decoration-double decoration-from-font decoration-teal-700  md:text-2xl font-bold text-xl  text-transparent  bg-clip-text bg-gradient-to-r from-emerald-700 via-sky-600 to-teal-800 '>
+                                        <p className='underline underline-offset-[4px] decoration-double decoration-from-font decoration-teal-700  md:text-2xl font-bold text-xl  text-transparent  bg-clip-text bg-gradient-to-r from-emerald-700 via-sky-600 to-teal-800 break-all outline-slate-950'>
                                             {post.title}
                                         </p>
                                         <p className='mt-3 text-gray-600 line-clamp-2'>
@@ -103,19 +103,19 @@ const Blogs = ({ posts }) => {
                 </div>
             </div>
         </Layout>
-    );
-};
+    )
+}
 
-export default Blogs;
+export default Blogs
 
-export async function getStaticProps({ locale }) {
+export async function getStaticProps ({locale}) {
     const query = groq`*[_type == "post" && language == $locale]{
     ...,
     author->,
     categories[]->
     | order(created_At desc)
-  }`;
-    const posts = await client.fetch(query, { locale });
+  }`
+    const posts = await client.fetch(query, {locale})
 
     return {
         props: {
@@ -123,5 +123,5 @@ export async function getStaticProps({ locale }) {
             posts,
         },
         revalidate: 60,
-    };
+    }
 }
