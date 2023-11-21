7b567d9c60e3ff7ee9cc40d14c320136a60fb79f
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-21 18:01:25 +0100

    feat(blogs): use the same slug for translation and add ISR to blogs
    use the same slug for translation and add ISR to blogs
    
    #35

--- sanity/schemas/post.js
+++ sanity/schemas/post.js
@@ -1,4 +1,4 @@
-import {apiVersion} from '../env'
+import { apiVersion } from "../env";
 export const post = {
     name: "post",
     title: "Post",
@@ -13,12 +13,13 @@ export const post = {
             name: "slug",
             title: "Slug",
             type: "slug",
-            readOnly: ({document}) => {return document?.language !== "en"},
+            readOnly: ({ document }) => {
+                return document?.language !== "en";
+            },
             options: {
                 source: "title",
                 maxLength: 96,
                 isUnique: isUniqueOtherThanLanguage,
-
             },
         },
         {
@@ -30,7 +31,7 @@ export const post = {
             name: "author",
             title: "Author",
             type: "reference",
-            to: {type: "author"},
+            to: { type: "author" },
         },
         {
             name: "mainImage",
@@ -46,13 +47,12 @@ export const post = {
                     title: "Alternative Text",
                 },
             ],
-
         },
         {
             name: "categories",
             title: "Categories",
             type: "array",
-            of: [{type: "reference", to: {type: "category"}}],
+            of: [{ type: "reference", to: { type: "category" } }],
         },
         {
             name: "publishedAt",
@@ -78,32 +78,31 @@ export const post = {
             author: "author.name",
             media: "mainImage",
         },
-        prepare (selection) {
-            const {author} = selection
-            return {...selection, subtitle: author && `by ${author}`}
+        prepare(selection) {
+            const { author } = selection;
+            return { ...selection, subtitle: author && `by ${author}` };
         },
     },
-}
+};
 
-
-export async function isUniqueOtherThanLanguage (slug, context) {
-    const {document, getClient} = context
+export async function isUniqueOtherThanLanguage(slug, context) {
+    const { document, getClient } = context;
     if (!document?.language) {
-        return true
+        return true;
     }
-    const client = getClient({apiVersion})
-    const id = document._id.replace(/^drafts\./, '')
+    const client = getClient({ apiVersion });
+    const id = document._id.replace(/^drafts\./, "");
     const params = {
         draft: `drafts.${id}`,
         published: id,
         language: document.language,
         slug,
-    }
+    };
     const query = `!defined(*[
     !(_id in [$draft, $published]) &&
     slug.current == $slug &&
     language == $language
-  ][0]._id)`
-    const result = await client.fetch(query, params)
-    return result
+  ][0]._id)`;
+    const result = await client.fetch(query, params);
+    return result;
 }
