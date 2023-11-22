4684558e46026df5754d0feaa4d57cdb4300f255
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-22 01:23:56 +0100

    fix(blogs): deploy sanity to netlify
    deploy sanity to netlify
    
    #35

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,11 @@
-import { createClient } from "next-sanity";
+import {createClient} from "next-sanity"
 
-import { apiVersion, dataset, projectId, useCdn } from "../env";
+import {apiVersion, dataset, projectId, token, useCdn} from "../env"
 
 export const client = createClient({
     apiVersion,
     dataset,
+    token,
     projectId,
     useCdn,
-});
+})
