1056aeee79887b76df1d840c7f69c5f9bf31447a
Lilia <liliabendjeddou9@gmail.com>
2023-11-24 14:52:52 +0100

    style(communication section): enhance the design

--- src/components/Cards/CommunicationCard.jsx
+++ src/components/Cards/CommunicationCard.jsx
@@ -18,16 +18,16 @@ export default function CommunicationCard({
     return (
         <Reveal>
             <div className='h-auto lg:h-[355px]'>
-                <div className='hover:scale-105 border border-gray p-1 h:44 w-56 lg:h-80 lg:w-60 rounded-3xl m-4 shadow-xl'>
+                <div className='hover:scale-105 border border-gray p-1 h-64 w-56 lg:h-80 lg:w-60 rounded-3xl m-4 shadow-xl'>
                     <div
-                        className={`flex flex-col h-44 w-[2356x] rounded-[20px]  ${greenBackground} `}
+                        className={`flex flex-col h-32 w-[215px] lg:h-44 lg:w-[230px] rounded-[20px]  ${greenBackground} `}
                     >
-                        <span className='text-6xl m-auto'>{IconComponent}</span>
-                        <h3 className='mx-auto text-3xl font-atkinson font- mb-8'>
+                        <span className='text-5xl lg:text-6xl m-auto'>{IconComponent}</span>
+                        <h3 className='mx-auto text-xl lg:text-3xl font-atkinson mb-4 lg:mb-8'>
                             {title}
                         </h3>
                     </div>
-                    <p className='font-atkinson text-center p-6 text-lg text-gray-700'>
+                    <p className='font-atkinson text-center p-6 text-md lg:text-lg text-gray-700'>
                         {paragraph}
                     </p>
                 </div>
