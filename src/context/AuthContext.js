a05d022c51d0e65004e97d61dbea2b3caf055caf
Lilia <liliabendjeddou9@gmail.com>
2023-11-27 19:00:43 +0100

    feat(auth context): save the google signup information in firestore

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,7 @@ import {
     updateProfile,
 } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where, setDoc } from "firebase/firestore";
 import Image from "next/image";
 import Spinner from "public/loading.svg";
 import Profile from "public/profile.png";
@@ -35,34 +35,34 @@ export function AppWrapper({ children }) {
     const facebookProvider = new FacebookAuthProvider();
 
     const AuthWithGoogle = () => {
-        // Implement Google login using Firebase here
-        signInWithPopup(auth, googleProvider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential =
-                    GoogleAuthProvider.credentialFromResult(result);
+        try {
+            signInWithPopup(auth, googleProvider).then(async (result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
-                // The signed-in user info.
                 const user = result.user;
-                console.log("google user", user);
-                // IdP data available using getAdditionalUserInfo(result)
-                // ...
-            })
-            .catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // console.log("can't log in", errorMessage, " ", errorCode);
-                // The email of the user's account used.
-                const email = error.customData.email;
-                // console.log("wrong email", email);
-                // The AuthCredential type that was used.
-                const credential =
-                    GoogleAuthProvider.credentialFromError(error);
-                // console.log("error", credential);
-                // ...
+    
+                // Save user information in Firestore
+                const userCollection = collection(db, 'users');
+                const userDocRef = doc(userCollection, user.uid);
+                const userDocSnapshot = await getDoc(userDocRef);
+    
+                if (!userDocSnapshot.exists()) {
+                    // If the document doesn't exist, create a new one
+                    await setDoc(userDocRef, {
+                        firstname: user.displayName.split(' ')[0],
+                        lastname: user.displayName.split(' ')[1],
+                        email: user.email,
+                        uid: user.uid,
+                        photoURL: user.photoURL
+                    });
+                }
+                setIsSignUpSuccessful(true);
             });
+        } catch (error) {
+            console.error('Error signing in with Google:', error);
+        }
     };
+
 
     const AuthWithFacebook = () => {
         // Implement Facebook login using Firebase here
