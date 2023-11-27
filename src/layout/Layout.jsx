68deafc42cb0fc3eb333ca6d62a9b2b5edb3a342
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-27 22:25:09 +0100

    fix(profile): fix profile buttons on small screen, add transition to toast

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,36 +1,42 @@
-import { motion } from "framer-motion";
-import { useRouter } from "next/router";
-import * as React from "react";
-import { ToastContainer } from "react-toastify";
+import {motion} from "framer-motion"
+import * as React from "react"
+import {Slide, ToastContainer} from "react-toastify"
 
-import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css"
 
-import ChatCard from "@/components/Cards/ChatCard";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar/Navbar";
+import ChatCard from "@/components/Cards/ChatCard"
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar/Navbar"
 
-export default function Layout({ children }) {
-    const router = useRouter();
-    const language = router.locale;
+export default function Layout ({children}) {
+
     return (
         <>
             <Navbar />
 
             <motion.div
-                initial={{ opacity: 0, y: 15 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 15 }}
-                transition={{ delay: 0.25 }}
+                initial={{opacity: 0, y: 15}}
+                animate={{opacity: 1, y: 0}}
+                exit={{opacity: 0, y: 15}}
+                transition={{delay: 0.25}}
             >
                 <ToastContainer
-                    rtl={language == "ar" && true}
-                    position={language == "ar" ? "top-right" : "top-left"}
-                    toastClassName='font-medium md:text-xl p-3 space-x-2 w-full'
+                    position="top-center"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    rtl={false}
+                    transition={Slide}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    toastClassName='font-medium md:text-lg p-3 space-x-2 w-full'
                 />
                 {children}
             </motion.div>
             <ChatCard />
             <Footer />
         </>
-    );
+    )
 }
