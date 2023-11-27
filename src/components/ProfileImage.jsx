722fee7bb1298c3189621b3bd9d3ae0e6e896cea
Walid Lamraoui <m4dcod3r@protonmail.com>
2023-11-28 00:44:56 +0100

    fix(profileimage.jsx): fix ProfileImage.jsx

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -1,45 +1,45 @@
-import {getDownloadURL, ref, uploadBytes} from "firebase/storage"
-import Image from "next/image"
-import {useTranslation} from "next-i18next"
-import Edit from "public/edit-profile.svg"
-import Profile from "public/profile.png"
-import Spinner from "public/spinner.svg"
-import {useState} from "react"
-import {toast} from "react-toastify"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Image from "next/image";
+import { useTranslation } from "next-i18next";
+import Edit from "public/edit-profile.svg";
+import Profile from "public/profile.png";
+import Spinner from "public/spinner.svg";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-import {useAuth} from "@/context/AuthContext"
-import {storage} from "@/util/firebase"
+import { useAuth } from "@/context/AuthContext";
+import { storage } from "@/util/firebase";
 const ProfileImage = () => {
-    const {t} = useTranslation("therapists")
-    const [uploading, setUploading] = useState(false)
-    const {user, updateProfilePhoto} = useAuth()
-    const [ProfileImage, setProfileImage] = useState("")
+    const { t } = useTranslation("therapists");
+    const [uploading, setUploading] = useState(false);
+    const { user, updateProfilePhoto } = useAuth();
+    const [ProfileImage, setProfileImage] = useState("");
     const handleUpload = async (e) => {
-        const file = e.target.files[0]
-        const imageName = `${user.uid}`
-        const path = "images/users/"
-        setProfileImage(URL.createObjectURL(file))
+        const file = e.target.files[0];
+        const imageName = `${user.uid}`;
+        const path = "images/users/";
+        setProfileImage(URL.createObjectURL(file));
         try {
-            if (!file) return
-            const imageRef = ref(storage, `${path}/${imageName}`)
+            if (!file) return;
+            const imageRef = ref(storage, `${path}/${imageName}`);
             try {
-                await uploadBytes(imageRef, file)
+                await uploadBytes(imageRef, file);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-            setUploading(true)
-            const profileImageRef = ref(storage, `${path}${imageName}`)
-            const downloadURL = await getDownloadURL(profileImageRef)
-            updateProfilePhoto(downloadURL)
-            setProfileImage(downloadURL)
-            localStorage.setItem(`profile_${user.uid}`, downloadURL)
+            setUploading(true);
+            const profileImageRef = ref(storage, `${path}${imageName}`);
+            const downloadURL = await getDownloadURL(profileImageRef);
+            updateProfilePhoto(downloadURL);
+            setProfileImage(downloadURL);
+            localStorage.setItem(`profile_${user.uid}`, downloadURL);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         } finally {
-            setUploading(false)
-            toast.success(t("profile.notifications.image"))
+            setUploading(false);
+            toast.success(t("profile.notifications.image"));
         }
-    }
+    };
     return (
         <>
             <div className='relative inline-flex justify-center w-[14rem] h-[14rem] lg:w-[20rem] lg:h-[20rem] xl:w-[18rem] xl:h-[18rem] rounded-full border-2 border-black '>
@@ -47,12 +47,10 @@ const ProfileImage = () => {
                     src={
                         uploading
                             ? Spinner
-                            : localStorage.getItem(
-                                `   profile_${user.uid}`
-                            ) ||
-                            user.photoURL ||
-                            ProfileImage ||
-                            Profile
+                            : localStorage.getItem(`   profile_${user.uid}`) ||
+                              user.photoURL ||
+                              ProfileImage ||
+                              Profile
                     }
                     alt='profile preview'
                     width={400}
@@ -73,7 +71,7 @@ const ProfileImage = () => {
                 </label>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default ProfileImage
+export default ProfileImage;
